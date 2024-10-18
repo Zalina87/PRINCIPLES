da41cdd8a5d086e343bbe592c97abf301c45f282
Zalina <salina87@yandex.ru>
2024-10-18 16:17:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/statistic/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.statistic;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public int FormDate;
+    public FormDate birthday;
+
+
+    // поле для имени
+
+}
